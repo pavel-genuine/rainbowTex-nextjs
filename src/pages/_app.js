ab6942e1183b1b46e8aacf5f598932befbb76f72
pavel.genuine <pavel.playstore@gmail.com>
2023-05-22 12:02:19 +0600

    fff

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -55,7 +55,7 @@ export default function App({ Component, pageProps }) {
         {
           !isOpen ?
             <div className=' h-[100vh] w-[100vw] flex justify-center items-center bg-bg'>
-              <div className='scale-[.5] flex justify-center items-center'>
+              <div className='lg:scale-[.5] scale-[.8] flex justify-center items-center'>
                 <motion.div
                   // viewport={{ once: true }}
                   initial={{ opacity: 0, scale: .3 }}
@@ -64,7 +64,7 @@ export default function App({ Component, pageProps }) {
 
                 >
                   <img
-                    className=''
+                    className='bg-[white] px-3 py-2 rounded-full bg-opacity-70'
                     src="https://i.ibb.co/nRLv9G8/rainbowtex-removebg-preview.png" alt="" srcset=""
                   />
                 </motion.div>
