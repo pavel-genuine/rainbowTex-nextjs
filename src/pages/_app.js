9dfef58282a22649672b15de039b76122c558d16
pavel.genuine <pavel.playstore@gmail.com>
2023-06-06 08:33:17 +0600

    ff

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -52,7 +52,7 @@ export default function App({ Component, pageProps }) {
 
     setTimeout(() => {
       setIsOpen(true)
-    }, 2000)
+    }, 20000)
 
   }, [])
 
@@ -66,57 +66,9 @@ export default function App({ Component, pageProps }) {
       <div className=''>
         {
           !isOpen ?
-            // <div className=' h-[100vh] w-[100vw] flex justify-center items-center bg-bg'>
-            //   <div className='lg:scale-[.5] scale-[.8] flex justify-center items-center'>
-            //     <motion.div
-            //       // viewport={{ once: true }}
-            //       initial={{ opacity: 0, scale: .3 }}
-            //       whileInView={{ opacity: 1, scale: 1 }}
-            //       transition={{ duration: 1.5, delay: 0 }}
-
-            //     >
-            //       <img
-            //         className='bg-[white] px-3 py-2 rounded-full bg-opacity-70'
-            //         src="https://i.ibb.co/nRLv9G8/rainbowtex-removebg-preview.png" alt="" srcset=""
-            //       />
-            //     </motion.div>
-
-            //   </div>
-            // </div>
-
-            // <div>
-            //   <div
-            //     className='h-[100vh] flex justify-center items-center bg-bg w-[100vw] overflow-hidden'
-
-            //   >
-            //     <motion.div
-            //       className='absolute top-50 z-10'
-            //       initial={{ opacity: 0, scale: .1 }}
-            //       whileInView={{ opacity: 1, scale: .5 }}
-            //       transition={{ duration: 2, delay: .2 }}
-
-            //     >
-            //       <img
-            //         // className='bg-[white grayscal px-3 py-2 rounded-full  bg-opacity-70'
-            //         src="https://i.ibb.co/djRrFXd/rainbowtex-removebg-preview-1.png" alt="" srcset=""
-            //       />
-            //     </motion.div>
-
-
-            //     <motion.div className='bg-[white] h-[100px] w-[100px] rounded-full z-0'
-
-            //       initial={{ opacity: 1, scale: .5 }}
-            //       whileInView={{ opacity: 1, scale: 100 }}
-            //       transition={{ duration: 3, delay: .2 }}
-
-            //     >
-
-            //     </motion.div>
-            //   </div>
-            // </div>
 
             <div className={roboto.className}>
-              <div className=' h-[100vh] w-[100vw] flex justify-center items-center bg-bg text-[white] text-center'>
+              <div className=' h-[100vh] w-[100vw] flex justify-center items-center bg-bg text-[white] text-center overflow-hidden'>
               <div className='scale-[.8] lg:scale-[1.5] lg:flex '>
 
                 <SunnyMorning text='Rainbow' />
